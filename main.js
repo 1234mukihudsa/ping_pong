0d0a1d18ac81e6e2a544ffe9b407244af906d92e
1234mukihudsa <94767139+1234mukihudsa@users.noreply.github.com>
2022-05-07 11:51:17 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -20,7 +20,10 @@ var ball = {
     dx:3,
     dy:3
 }
-
+rigthWristX=0;
+rigthWristY=0;
+scoreRightWrist=0;
+game_status="";
 function setup(){
   var canvas =  createCanvas(700,600);
   canvas.parent('canvas');
@@ -35,12 +38,21 @@ function modelLoaded()
   console.log("model is loaded");
 
 }
-
+function gotPoses(results){
+  if(results.length>0){
+    rigthWristX=results[0].pose.rightWrist.x;
+    rigthWristY=results[0].pose.rightWrist.y;
+    scoreRightWrist=results[0].pose.keypoints[10].score;
+    console.log(scoreRightWrist);
+  }
+}
 
 function draw(){
+if(game_status=="start"){
+  
 
  background(0); 
-
+image(video,0,0,700,600);
  fill("black");
  stroke("black");
  rect(680,0,20,700);
@@ -48,7 +60,11 @@ function draw(){
  fill("black");
  stroke("black");
  rect(0,0,20,700);
- 
+ if(scoreRightWrist>0.2){
+   fill("red");
+   stroke("red");
+   circle(rightWristX,rigthWristY,30);
+ }
    //funtion paddleInCanvas call 
    paddleInCanvas();
  
@@ -76,6 +92,7 @@ function draw(){
    
    //function move call which in very important
     move();
+}
 }
 
 
